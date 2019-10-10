ac5571c53bc0a6bdbe94a7305e0711aad62b5f74
Christian Ferber <cferber@vmware.com>
2019-10-10 11:29:43 +0200

    changed hello name

--- CF Hello World.py
+++ CF Hello World.py
@@ -1,5 +1,5 @@
 def handler(context, inputs):
-    greeting = "Hello, {0} from Christian!".format(inputs["target"])
+    greeting = "Hello, {0} from the best friend!".format(inputs["target"])
     print(greeting)
 
     outputs = {
